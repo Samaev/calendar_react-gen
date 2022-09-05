e8808ef00a753de0e1fa8cd77876e92c23d6a9d2
Andrey Samaev <andreysamaev@gmail.com>
2022-09-05 21:25:39 +0300

    react implemented, tailwind, craco

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { getMonth } from '../src/utils/utils';
+import Sidebar from './components/Sidebar';
+import Month from './components/Month';
+import CalendarHeader from './components/CalendarHeader';
+import React from 'react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <React.Fragment>
+    <div className="h-screen flex flex-columns">
+      <CalendarHeader />
+      <div className="flex flex-1">
+        <Sidebar />
+        <Month />
+      </div>
     </div>
+   </React.Fragment>
   );
 }
 
